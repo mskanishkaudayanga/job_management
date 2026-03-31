06e46ae8377cacea524fc0ec22f4c963d8cedcf1
mskanishkaudayanga <mskanishkaudayanga@gmail.com>
2026-04-01 00:18:09 +0530

    add main

---
+++ Job-queues-core/src/main/java/com/pm/job_queues/JobQueuesApplication.java
@@ -0,0 +1,17 @@
+package com.pm.job_queues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableAspectJAutoProxy
+public class JobQueuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobQueuesApplication.class, args);
+    }
+
+}
