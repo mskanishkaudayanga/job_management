acb2a38f2a3134ed24fd20ea061f8d033d909c7b
mskanishkaudayanga <mskanishkaudayanga@gmail.com>
2026-04-25 15:16:02 +0530

    directory change

--- Job-queues-core/src/main/java/com/pm/job_queues/handlers/JobHandler.java
+++
@@ -1,7 +0,0 @@
-package com.pm.job_queues.handlers;
-
-import com.pm.job_queues.model.Job;
-
-public interface JobHandler {
-    void handle(Job job);
-}
--- Job-queues-core/src/main/java/com/pm/job_queues/queue/JobQueue.java
+++
@@ -1,8 +0,0 @@
-package com.pm.job_queues.queue;
-
-import com.pm.job_queues.model.Job;
-
-public interface JobQueue {
-    void enqueue(String queueName, Job job);
-    Job dequeue(String queueName);
-}
